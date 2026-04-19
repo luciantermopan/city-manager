5cfbec8c7e9390f1101ef09b96aeace52954e26f
luciantermopan <y4b8j1@gmail.com>
2026-04-19 23:42:54 +0300

    write capability reports.dat file

--- main.c
+++ main.c
@@ -5,8 +5,52 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
-void add(char *district_id)
+typedef struct report
+{
+    char inspector_name[20];
+    char category[20];
+    char description[100];
+    int id;
+    time_t timestamp;
+    float GPS_N, GPS_E;
+    int severity_level; // integer: 1 = minor, 2 = moderate, 3 = critical
+}Report;
+
+int create_dir_and_set_perm(const char *district_id)
+{
+    printf("%s\n", district_id);
+
+    char path[100];
+
+    // make directory, have to check if it created successfully
+    if(mkdir(district_id, 0750) != 0)
+    {
+        perror("Directory could not be created\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // int open(const char *pathname, int flags, mode_t mode);
+    sprintf(path, "%s/reports.dat", district_id);
+    int fd = 0;
+    fd = open(path, O_CREAT | O_APPEND | O_RDWR,0664); // create 'reports.dat' file
+    close(fd);
+
+    sprintf(path, "%s/district.cfg", district_id);
+    fd = open(path, O_CREAT | O_RDWR, 0640);
+    close(fd);
+
+    sprintf(path, "%s/logged_district", district_id);
+    fd = open(path, O_CREAT, 0644);
+    close(fd);
+
+
+    return 0;
+}
+
+void add(char *district_id, Report rep)
 {
     // parse each district directory for its id and
     // check if it's the right one
@@ -16,43 +60,57 @@ void add(char *district_id)
 
     struct dirent *directory_element = NULL;
 
+    int dist_exists = 0;
     while((directory_element = readdir(current_dir)) != NULL)
     {
-        // printf("%s\n", directory_element->d_name);
         // for each element we must check if it is a folder (different from . and ..) (and not a symbolic link)
 
         struct stat file;
-        lstat(directory_element->d_name, &file);
-        // check if element is a folder
+        lstat(directory_element->d_name, &file); // extract info about each file in the containing directory
+
+        // check if 'element' is a folder
         if(S_ISDIR(file.st_mode)==1 && (strcmp("..", directory_element->d_name)!=0 && strcmp(".", directory_element->d_name)!=0))
         {
-            if(strcmp(district_id, directory_element->d_name)!=0) // district directory does not exist
+            // district directory exists
+            if(strcmp(district_id, directory_element->d_name) == 0)
             {
-                // needs to be created
-                printf("%d\n",   mkdir(district_id, 0750));
-                chdir(district_id);
-                open("reports.dat",O_RDWR, O_CREAT | O_APPEND);
-                open("district.cfg", O_RDWR, O_CREAT);
-                open("logged_district", O_RDWR, O_CREAT);
-                chdir("..");
+                dist_exists = 1;
+                // open files and write to them...
+                char path[100];
+                sprintf(path, "%s/reports.dat", district_id);
+                int fd_reports = open(path, O_RDWR | O_APPEND);
+                write(fd_reports, &rep, sizeof(Report));
+                close(fd_reports);
             }
-            else // district directory exists, now only need to add the report under reports.dat
-            {
 
-            }
         }
     }
-
-
+    // directory does not exist, we have to create it and its files
+    if(dist_exists == 0)
+    {
+        create_dir_and_set_perm(district_id);
+    }
 
     // close the containing directory
-    if(current_dir!= NULL)
+    if(current_dir != NULL)
         closedir(current_dir);
     return;
 }
 
 int main(int argc, char *argv[])
 {
-    add(argv[1]);
+    // create a fake report for now..
+    Report r1 = {0};
+    strcpy(r1.inspector_name, "tester");
+    strcpy(r1.category, "road");
+    strcpy(r1.description, "pothole");
+    r1.id = rand() % 1000;
+    r1.timestamp = time(NULL);
+    r1.GPS_N = 44.4268;
+    r1.GPS_E = 26.1025;
+    r1.severity_level = 2;
+    
+    add(argv[1], r1);
+
     return 0;
 }
